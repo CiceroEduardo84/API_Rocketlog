761ed0dc34f3e337566dee6b474122e2ca3a4573
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2025-12-18 23:02:42 -0300

    add sessions controller

--- src/controllers/sessionsController.ts
+++ src/controllers/sessionsController.ts
@@ -1,8 +1,42 @@
+import { authConfig } from "@/config/auth";
+import { prisma } from "@/database/prisma";
+import { AppError } from "@/utils/AppError";
+import { compare } from "bcrypt";
 import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { z } from "zod";
 
 class SessionsController {
-  create(request: Request, response: Response) {
-    return response.json({ message: "OK" });
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      email: z.string().email(),
+      password: z.string().min(6),
+    });
+
+    const { email, password } = bodySchema.parse(request.body);
+
+    const user = await prisma.users.findFirst({ where: { email } });
+
+    if (!user) {
+      throw new AppError("Invalide email or password", 401);
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new AppError("Invalide email or password", 401);
+    }
+
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = jwt.sign({ role: user.role ?? "customer" }, secret, {
+      subject: user.id,
+      expiresIn,
+    });
+
+    const {password:hashedPassword, ...userWithoutPassword} = user
+
+    return response.json({token,  user: userWithoutPassword});
   }
 }
 
