fa8a5f993e84119f6106dc5358be19b9344c1e24
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2025-12-25 22:20:05 -0300

    add deliveries routes

--- src/routes/deliveries.routes.ts
+++ src/routes/deliveries.routes.ts
@@ -1,13 +1,18 @@
 import { DeliveriesControllers } from "@/controllers/deliveriesControllers";
+import { DeliveriesStatusControllers } from "@/controllers/deliveriesStatusControllers";
 import { ensureAuthenticated } from "@/middlewares/ensureAuthenticated";
-import { verifyUserAuthorization } from "@/middlewares/verifyUserauthorization";
+import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization";
 import { Router } from "express";
 
 const deliveriesRoutes = Router();
 
 const deliveriesControllers = new DeliveriesControllers();
+const deliveriesStatusControllers = new DeliveriesStatusControllers();
 
 deliveriesRoutes.use(ensureAuthenticated, verifyUserAuthorization(["sale"]));
 deliveriesRoutes.post("/", deliveriesControllers.create);
+deliveriesRoutes.get("/", deliveriesControllers.index);
+
+deliveriesRoutes.patch("/:id/status", deliveriesStatusControllers.update);
 
 export { deliveriesRoutes };
