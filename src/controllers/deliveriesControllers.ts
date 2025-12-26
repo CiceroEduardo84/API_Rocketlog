9e8d44224647d6b96caa9ed3831fffb7e522289e
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2025-12-25 22:19:48 -0300

    add deliveries controllers

--- src/controllers/deliveriesControllers.ts
+++ src/controllers/deliveriesControllers.ts
@@ -1,8 +1,39 @@
+import { prisma } from "@/database/prisma";
 import { Request, Response } from "express";
+import { z } from "zod";
 
 class DeliveriesControllers {
-  create(request: Request, response: Response) {
-    return response.json({ message: "OK" });
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      user_id: z.string(),
+      description: z.string(),
+    });
+
+    const { user_id, description } = bodySchema.parse(request.body);
+
+    await prisma.delivery.create({
+      data: {
+        userID: user_id,
+        description,
+      },
+    });
+
+    return response.status(201).json();
+  }
+
+  async index(request: Request, response: Response) {
+    const deliveries = await prisma.delivery.findMany({
+      include: {
+        user: {
+          select: {
+            name: true,
+            email: true,
+          },
+        },
+      },
+    });
+
+    return response.json(deliveries);
   }
 }
 
