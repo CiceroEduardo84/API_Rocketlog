851c25f31819360c79dd1827fcaa51013860201f
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2025-12-29 23:45:11 -0300

    fix: show delivery not found case not exist

--- src/controllers/deliveryLogsControllers.ts
+++ src/controllers/deliveryLogsControllers.ts
@@ -53,6 +53,10 @@ class DeliveryLogsControllers {
       },
     });
 
+    if (!delivery) {
+      return response.status(404).json({message:"Delivery not found!"})
+    }
+
     if (
       request.user?.role === "customer" &&
       request.user.id != delivery?.userID
